8b599467f8d4ebbfbb70e1219c5c8cc2a50ebdb6
SparklingTitan <SparklingTitan@ODESZA>
2025-07-08 10:33:31 +0200

    Projektdateien hinzufügen.

---
+++ Guild Wars 2 - Auction House Calculator/Main_Form.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Guild_Wars_2___Auction_House_Calculator
+{
+    partial class Main_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main_Form));
+            groupBox1 = new GroupBox();
+            Button_Wiki = new Button();
+            Button_Save = new Button();
+            Button_Load = new Button();
+            groupBox2 = new GroupBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox3 = new GroupBox();
+            DataGrid_Calculation = new DataGridView();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            pictureBox1 = new PictureBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)DataGrid_Calculation).BeginInit();
+            tabControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(Button_Wiki);
+            groupBox1.Controls.Add(Button_Save);
+            groupBox1.Controls.Add(Button_Load);
+            groupBox1.Location = new Point(1955, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(227, 372);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Menu";
+            // 
+            // Button_Wiki
+            // 
+            Button_Wiki.Location = new Point(6, 70);
+            Button_Wiki.Name = "Button_Wiki";
+            Button_Wiki.Size = new Size(206, 34);
+            Button_Wiki.TabIndex = 4;
+            Button_Wiki.Text = "Wiki";
+            Button_Wiki.UseVisualStyleBackColor = true;
+            Button_Wiki.Click += Button_Wiki_Click;
+            // 
+            // Button_Save
+            // 
+            Button_Save.Location = new Point(112, 30);
+            Button_Save.Name = "Button_Save";
+            Button_Save.Size = new Size(100, 34);
+            Button_Save.TabIndex = 1;
+            Button_Save.Text = "Save";
+            Button_Save.UseVisualStyleBackColor = true;
+            Button_Save.Click += Button_Save_Click;
+            // 
+            // Button_Load
+            // 
+            Button_Load.Location = new Point(6, 30);
+            Button_Load.Name = "Button_Load";
+            Button_Load.Size = new Size(100, 34);
+            Button_Load.TabIndex = 0;
+            Button_Load.Text = "Load";
+            Button_Load.UseVisualStyleBackColor = true;
+            Button_Load.Click += Button_Load_Click;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(label1);
+            groupBox2.Location = new Point(1955, 390);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(583, 567);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Additional Information";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(0, 357);
+            label3.Name = "label3";
+            label3.Size = new Size(509, 50);
+            label3.TabIndex = 2;
+            label3.Text = "ℹ️Data is saved automatically on close and loaded on startup.\r\nℹ️Calculation is done automatically after entering data.\r\n";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 191);
+            label2.Name = "label2";
+            label2.Size = new Size(456, 150);
+            label2.TabIndex = 1;
+            label2.Text = resources.GetString("label2.Text");
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(322, 150);
+            label1.TabIndex = 0;
+            label1.Text = "\U0001f9ee Fee Structure:\r\n\r\nThe Trading Post charges two fees:\r\n- Listing Fee: 5% of the sell price\r\n- Transaction Fee: 15% of the sell price .\r\n- Total Fees = 20% of the sell Price.";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(DataGrid_Calculation);
+            groupBox3.Location = new Point(12, 12);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(1937, 945);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Calculation";
+            // 
+            // DataGrid_Calculation
+            // 
+            DataGrid_Calculation.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            DataGrid_Calculation.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            DataGrid_Calculation.BackgroundColor = SystemColors.ScrollBar;
+            DataGrid_Calculation.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DataGrid_Calculation.Dock = DockStyle.Fill;
+            DataGrid_Calculation.Location = new Point(3, 27);
+            DataGrid_Calculation.Name = "DataGrid_Calculation";
+            DataGrid_Calculation.RowHeadersWidth = 62;
+            DataGrid_Calculation.Size = new Size(1931, 915);
+            DataGrid_Calculation.TabIndex = 0;
+            DataGrid_Calculation.CellEndEdit += DataGrid_Calculation_CellEndEdit;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(525, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(8, 8);
+            tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Location = new Point(4, 34);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(0, 0);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 34);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(0, 0);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.GW2_Auction_House_Calculator;
+            pictureBox1.Location = new Point(2188, 34);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(350, 350);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // Main_Form
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(2549, 968);
+            Controls.Add(pictureBox1);
+            Controls.Add(tabControl1);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Main_Form";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Guild Wars 2 – Auction House Calculator";
+            FormClosing += Main_Form_FormClosing;
+            Load += Main_Form_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)DataGrid_Calculation).EndInit();
+            tabControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+        private Button Button_Wiki;
+        private Button Button_Save;
+        private Button Button_Load;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private DataGridView DataGrid_Calculation;
+        private Label label1;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private Label label3;
+    }
+}
---
+++ Guild Wars 2 - Auction House Calculator/Main_Form.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guild_Wars_2___Auction_House_Calculator
+{
+    public partial class Main_Form : Form
+    {
+        public Main_Form()
+        {
+            InitializeComponent();
+        }
+        private void Main_Form_Load(object sender, EventArgs e)
+        {
+            // Set the DataGridView properties
+            DataGrid_Calculation.AutoGenerateColumns = false;
+            DataGrid_Calculation.AllowUserToAddRows = true;
+            DataGrid_Calculation.AllowUserToDeleteRows = true;
+            // Add columns to the DataGridView
+            DataGrid_Calculation.Columns.Add(new DataGridViewTextBoxColumn { Name = "Item Name", HeaderText = "Item Name" });
+
+            var rarityColumn = new DataGridViewComboBoxColumn
+            {
+                Name = "Rarity",
+                HeaderText = "Rarity",
+                DataSource = new string[]
+                {
+                    "Junk",
+                    "Basic",
+                    "Fine",
+                    "Masterwork",
+                    "Rare",
+                    "Exotic",
+                    "Ascended",
+                    "Legendary"
+                },
+                FlatStyle = FlatStyle.Flat
+            };
+            DataGrid_Calculation.Columns.Add(rarityColumn);
+            DataGrid_Calculation.CellFormatting += (s, e) =>
+            {
+                if (DataGrid_Calculation.Columns[e.ColumnIndex].Name == "Rarity" && e.Value is string rarity)
+                {
+                    e.CellStyle.Font = new Font(DataGrid_Calculation.DefaultCellStyle.Font, FontStyle.Bold);
+                    e.CellStyle.ForeColor = GetRarityColor(rarity);
+                }
+            };
+            DataGrid_Calculation.Columns.Add(new DataGridViewTextBoxColumn { Name = "Buy Price", HeaderText = "Buy Price" });
+            DataGrid_Calculation.Columns.Add(new DataGridViewTextBoxColumn { Name = "Buy Quantity", HeaderText = "Buy Quantity" });
+            DataGrid_Calculation.Columns.Add(new DataGridViewTextBoxColumn { Name = "Sell Price", HeaderText = "Sell Price" });
+            DataGrid_Calculation.Columns.Add(new DataGridViewTextBoxColumn { Name = "Sell Quantity", HeaderText = "Sell Quantity" });
+            DataGrid_Calculation.Columns.Add(new DataGridViewTextBoxColumn { Name = "Total Buy Cost", HeaderText = "Total Buy Cost", ReadOnly = true });
+            DataGrid_Calculation.Columns.Add(new DataGridViewTextBoxColumn { Name = "Total Listing Fee", HeaderText = "Total Listing Fee", ReadOnly = true });
+            DataGrid_Calculation.Columns.Add(new DataGridViewTextBoxColumn { Name = "Total Transaction Fee", HeaderText = "Total Transaction Fee", ReadOnly = true });
+            DataGrid_Calculation.Columns.Add(new DataGridViewTextBoxColumn { Name = "Total Fees", HeaderText = "Total Fees", ReadOnly = true });
+            DataGrid_Calculation.Columns.Add(new DataGridViewTextBoxColumn { Name = "Total Revenue", HeaderText = "Total Revenue", ReadOnly = true });
+            DataGrid_Calculation.CellFormatting += (s, e) =>
+            {
+                var colName = DataGrid_Calculation.Columns[e.ColumnIndex].Name;
+
+                if (colName == "Rarity" && e.Value is string rarity)
+                {
+                    e.CellStyle.Font = new Font(DataGrid_Calculation.DefaultCellStyle.Font, FontStyle.Bold);
+                    e.CellStyle.ForeColor = GetRarityColor(rarity);
+                }
+
+                if ((colName == "Total Profit" || colName == "Profit Margin (%)") && e.Value != null && decimal.TryParse(e.Value.ToString(), out decimal val))
+                {
+                    e.CellStyle.Font = new Font(DataGrid_Calculation.DefaultCellStyle.Font, FontStyle.Bold);
+                    if (val < 0)
+                    {
+                        e.CellStyle.ForeColor = Color.Red;
+                    }
+                    else if (val > 0)
+                    {
+                        e.CellStyle.ForeColor = Color.Green;
+                    }
+                    else
+                    {
+                        e.CellStyle.ForeColor = DataGrid_Calculation.DefaultCellStyle.ForeColor;
+                    }
+                }
+            };
+            DataGrid_Calculation.Columns.Add(new DataGridViewTextBoxColumn { Name = "Total Profit", HeaderText = "Total Profit", ReadOnly = true });
+            DataGrid_Calculation.Columns.Add(new DataGridViewTextBoxColumn { Name = "Profit Margin (%)", HeaderText = "Profit Margin (%)", ReadOnly = true });
+
+
+            // Autoload last CSV data (autosave)
+            try
+            {
+                string autosavePath = System.IO.Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                    "GW2_AuctionHouseCalculator",
+                    "autosave.csv"
+                );
+
+                if (System.IO.File.Exists(autosavePath))
+                {
+                    var lines = System.IO.File.ReadAllLines(autosavePath, Encoding.UTF8);
+                    if (lines.Length > 0)
+                    {
+                        int startLine = 0;
+                        if (lines[0].Contains("Item Name") && lines[0].Contains("Rarity"))
+                            startLine = 1;
+
+                        DataGrid_Calculation.Rows.Clear();
+
+                        for (int i = startLine; i < lines.Length; i++)
+                        {
+                            var fields = lines[i].Split(';');
+                            if (fields.Length < DataGrid_Calculation.Columns.Count)
+                                continue; // Skip row if not enough columns
+
+                            int rowIdx = DataGrid_Calculation.Rows.Add();
+                            var row = DataGrid_Calculation.Rows[rowIdx];
+
+                            for (int col = 0; col < DataGrid_Calculation.Columns.Count; col++)
+                            {
+                                row.Cells[col].Value = string.IsNullOrWhiteSpace(fields[col]) ? null : fields[col];
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Ignore errors when loading to not prevent startup
+                Debug.WriteLine($"Error while auto-loading CSV file: {ex.Message}");
+            }
+        }
+        private void Button_Wiki_Click(object sender, EventArgs e)
+        {
+            string url = "https://wiki.guildwars2.com/";
+            try
+            {
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                };
+                Process.Start(psi);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Could not open the Webiste: {ex.Message}");
+
+            }
+        }
+        private void Button_Save_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "CSV-Dateien (*.csv)|*.csv|Alle Dateien (*.*)|*.*";
+                saveFileDialog.Title = "CSV-Datei speichern";
+                saveFileDialog.DefaultExt = "csv";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        var sb = new StringBuilder();
+
+                        // Header schreiben
+                        var header = DataGrid_Calculation.Columns
+                            .Cast<DataGridViewColumn>()
+                            .Select(col => col.HeaderText.Replace(";", ","));
+                        sb.AppendLine(string.Join(";", header));
+
+                        // Zeilen schreiben
+                        foreach (DataGridViewRow row in DataGrid_Calculation.Rows)
+                        {
+                            if (row.IsNewRow) continue;
+
+                            var fields = row.Cells
+                                .Cast<DataGridViewCell>()
+                                .Select(cell => (cell.Value ?? "").ToString().Replace(";", ","));
+                            sb.AppendLine(string.Join(";", fields));
+                        }
+
+                        System.IO.File.WriteAllText(saveFileDialog.FileName, sb.ToString(), Encoding.UTF8);
+                        MessageBox.Show("CSV-Datei erfolgreich gespeichert.", "Erfolg", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Fehler beim Speichern der CSV-Datei: {ex.Message}", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+        private void Button_Load_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "CSV-Dateien (*.csv)|*.csv|Alle Dateien (*.*)|*.*";
+                openFileDialog.Title = "CSV-Datei laden";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        var lines = System.IO.File.ReadAllLines(openFileDialog.FileName, Encoding.UTF8);
+                        if (lines.Length == 0) return;
+
+                        // Optional: Header prüfen
+                        int startLine = 0;
+                        if (lines[0].Contains("Item Name") && lines[0].Contains("Rarity"))
+                            startLine = 1;
+
+                        DataGrid_Calculation.Rows.Clear();
+
+                        for (int i = startLine; i < lines.Length; i++)
+                        {
+                            var fields = lines[i].Split(';');
+                            if (fields.Length < DataGrid_Calculation.Columns.Count)
+                                continue; // Zeile überspringen, wenn zu wenig Spalten
+
+                            int rowIdx = DataGrid_Calculation.Rows.Add();
+                            var row = DataGrid_Calculation.Rows[rowIdx];
+
+                            for (int col = 0; col < DataGrid_Calculation.Columns.Count; col++)
+                            {
+                                // Versuche, leere Felder als null zu setzen
+                                row.Cells[col].Value = string.IsNullOrWhiteSpace(fields[col]) ? null : fields[col];
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Fehler beim Laden der CSV-Datei: {ex.Message}", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+        private Color GetRarityColor(string rarity)
+        {
+            return rarity switch
+            {
+                "Junk" => Color.Gray,
+                "Basic" => Color.Black,
+                "Fine" => Color.Blue,
+                "Masterwork" => Color.Green,
+                "Rare" => Color.Gold,
+                "Exotic" => Color.Orange,
+                "Ascended" => Color.HotPink,
+                "Legendary" => Color.Purple,
+                _ => Color.Black
+            };
+        }
+        private void Main_Form_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                string autosavePath = System.IO.Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                    "GW2_AuctionHouseCalculator",
+                    "autosave.csv"
+                );
+
+                // Ensure the directory exists
+                System.IO.Directory.CreateDirectory(System.IO.Path.GetDirectoryName(autosavePath));
+
+                var sb = new StringBuilder();
+
+                // Write header
+                var header = DataGrid_Calculation.Columns
+                    .Cast<DataGridViewColumn>()
+                    .Select(col => col.HeaderText.Replace(";", ","));
+                sb.AppendLine(string.Join(";", header));
+
+                // Write rows
+                foreach (DataGridViewRow row in DataGrid_Calculation.Rows)
+                {
+                    if (row.IsNewRow) continue;
+
+                    var fields = row.Cells
+                        .Cast<DataGridViewCell>()
+                        .Select(cell => (cell.Value ?? "").ToString().Replace(";", ","));
+                    sb.AppendLine(string.Join(";", fields));
+                }
+
+                System.IO.File.WriteAllText(autosavePath, sb.ToString(), Encoding.UTF8);
+            }
+            catch (Exception ex)
+            {
+                // Ignore autosave errors to not prevent closing
+                Debug.WriteLine($"Autosave error: {ex.Message}");
+            }
+        }
+        private void DataGrid_Calculation_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            foreach (DataGridViewRow row in DataGrid_Calculation.Rows)
+            {
+                if (row.IsNewRow) continue;
+
+                // Werte auslesen und validieren
+                bool validBuyPrice = decimal.TryParse(row.Cells["Buy Price"].Value?.ToString(), out decimal buyPrice);
+                bool validBuyQty = decimal.TryParse(row.Cells["Buy Quantity"].Value?.ToString(), out decimal buyQty);
+                bool validSellPrice = decimal.TryParse(row.Cells["Sell Price"].Value?.ToString(), out decimal sellPrice);
+                bool validSellQty = decimal.TryParse(row.Cells["Sell Quantity"].Value?.ToString(), out decimal sellQty);
+
+                if (!(validBuyPrice && validBuyQty && validSellPrice && validSellQty))
+                {
+                    // Ungültige Werte: Felder leeren
+                    row.Cells["Total Buy Cost"].Value = null;
+                    row.Cells["Total Listing Fee"].Value = null;
+                    row.Cells["Total Transaction Fee"].Value = null;
+                    row.Cells["Total Fees"].Value = null;
+                    row.Cells["Total Revenue"].Value = null;
+                    row.Cells["Total Profit"].Value = null;
+                    row.Cells["Profit Margin (%)"].Value = null;
+                    continue;
+                }
+
+                // Berechnungen
+                decimal totalBuyCost = buyPrice * buyQty;
+                decimal totalListingFee = sellPrice * sellQty * 0.05m;
+                decimal totalTransactionFee = sellPrice * sellQty * 0.15m;
+                decimal totalFees = totalListingFee + totalTransactionFee;
+                decimal totalRevenue = sellPrice * sellQty * 0.8m;
+                decimal totalProfit = totalRevenue - totalBuyCost;
+                decimal profitMargin = totalBuyCost != 0 ? (totalProfit / totalBuyCost) * 100 : 0;
+
+                // Ergebnisse eintragen (auf 2 Nachkommastellen runden)
+                row.Cells["Total Buy Cost"].Value = Math.Round(totalBuyCost, 2);
+                row.Cells["Total Listing Fee"].Value = Math.Round(totalListingFee, 2);
+                row.Cells["Total Transaction Fee"].Value = Math.Round(totalTransactionFee, 2);
+                row.Cells["Total Fees"].Value = Math.Round(totalFees, 2);
+                row.Cells["Total Revenue"].Value = Math.Round(totalRevenue, 2);
+                row.Cells["Total Profit"].Value = Math.Round(totalProfit, 2);
+                row.Cells["Profit Margin (%)"].Value = Math.Round(profitMargin, 2);
+            }
+        }
+    }
+}
